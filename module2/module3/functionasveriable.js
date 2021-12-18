578b3e2c1624d78e5103d3b9eb4f6cff9d2257ba
subhransudhal <rkl_subhransu@yahoo.in>
2021-12-17 20:24:39 -0800

    js

--- module2/module3/functionasveriable.js
+++ module2/module3/functionasveriable.js
@@ -1,9 +1,10 @@
-
+//anonymus function as veiable
 
 const functionprint = function(num1,num2){
     console.log(num1+num2);
 }
-
+// var= which can change.
+//const= which can not change.
 
 //console.log(functionprint(10,20));
 functionprint(10,20);
